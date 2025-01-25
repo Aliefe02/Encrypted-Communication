a0bc74bcbf06741e349778a38b9e906574ddb214
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2025-01-25 20:15:47 +0300

    Update lora.py

--- LoRa Communication/lora.py
+++ LoRa Communication/lora.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-MSG_SIZE = 250
+MSG_SIZE = 245
 
 class LoRa:
     def __init__(self, baudrate, port):
@@ -11,22 +11,24 @@ class LoRa:
         time.sleep(3)
 
     def send_message(self, message):
-        message_to_send = message + "send\n"
 
-        message_chunks = [message_to_send[i:i + MSG_SIZE] for i in range(0, len(message), MSG_SIZE)]
+        message_chunks = [message[i:i + MSG_SIZE] for i in range(0, len(message), MSG_SIZE)]
         
         message_chunks_len = len(message_chunks)
 
         if message_chunks_len > 1:
+            print(f"%0 completed", end='\r')
             for i in range(message_chunks_len):
-                print(f"%{round((i+1)*100/message_chunks_len,2)} complete", end='\r')
+                
+                self.ser.write((message_chunks[i]+'send\n').encode())
                 if not self.waitACK():
                     return False
-                self.ser.write(message_chunks[i].encode())
+                print(f"%{int((i+1)*100/message_chunks_len)} complete", end='\r')
             print(f"%100 completed")
+            return True
 
         else:
-            self.ser.write(message_chunks[0].encode())
+            self.ser.write((message_chunks[0]+'send\n').encode())
             return self.waitACK()
 
     def receive_message(self):
@@ -44,6 +46,9 @@ class LoRa:
 
     def waitACK(self):
         while True:
-            if self.ser.in_waiting >= 2:
-                msg = self.ser.read(2).decode('utf-8')
-                return msg == 'ok'                
+            msg = ''
+            if self.ser.in_waiting >= 0:
+                while self.ser.in_waiting >= 0:    
+                    msg += self.ser.read(self.ser.in_waiting).decode('utf-8').replace(" ", "").replace("\n", "")
+                    if len(msg) >= 2:
+                        return msg[:2] == 'ok'
