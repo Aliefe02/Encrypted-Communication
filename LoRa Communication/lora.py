fb17b9990a822fea7df88beddf1a3d58cd100d86
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2025-01-25 18:45:19 +0300

    Update lora.py

--- LoRa Communication/lora.py
+++ LoRa Communication/lora.py
@@ -1,22 +1,49 @@
 import serial
+import time
+
+MSG_SIZE = 250
 
 class LoRa:
     def __init__(self, baudrate, port):
         self.baudrate = baudrate
         self.port = port
         self.ser = serial.Serial(self.port, self.baudrate)
+        time.sleep(3)
 
     def send_message(self, message):
         message_to_send = message + "send\n"
-        self.ser.write(message_to_send.encode())
+
+        message_chunks = [message_to_send[i:i + MSG_SIZE] for i in range(0, len(message), MSG_SIZE)]
+        
+        message_chunks_len = len(message_chunks)
+
+        if message_chunks_len > 1:
+            for i in range(message_chunks_len):
+                print(f"%{round((i+1)*100/message_chunks_len,2)} complete", end='\r')
+                if not self.waitACK():
+                    return False
+                self.ser.write(message_chunks[i].encode())
+            print(f"%100 completed")
+
+        else:
+            self.ser.write(message_chunks[0].encode())
+            return self.waitACK()
 
     def receive_message(self):
         self.ser.write("recv\n".encode())
 
+        data = ''
         while True:
             if self.ser.in_waiting > 0:
-                return self.ser.readline().decode().strip()
+                data += self.ser.read(self.ser.in_waiting).decode('utf-8')
+                if data[-9:] == "[__END__]":
+                    return data 
             
     def close(self):
         self.ser.close()
-                
+
+    def waitACK(self):
+        while True:
+            if self.ser.in_waiting >= 2:
+                msg = self.ser.read(2).decode('utf-8')
+                return msg == 'ok'                
